0d2e38192c5ffb26861987086eba6e8682bf4dcf
oatspear <oatspear@gmail.com>
2023-07-24 21:10:51 +0100

    WIP logic

--- src/logic.ts
+++ src/logic.ts
@@ -1,12 +1,93 @@
+// -----------------------------------------------------------------------------
+// Imports
+// -----------------------------------------------------------------------------
+
 import type { RuneClient } from "rune-games-sdk/multiplayer"
+
+// -----------------------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------------------
+
+export type TaskType = 1 | 2;
+
+
+function constTaskTypeBookPurchase() { return 1 }
+function constTaskTypeBookReturn() { return 2 }
+
+
+// -----------------------------------------------------------------------------
+// Player State
+// -----------------------------------------------------------------------------
+
+
+export interface PlayerState {
+  id: string;
+  score: number;
+}
+
+
+function newPlayer(id: string): PlayerState {
+  return {
+    id,
+    score: 0,
+  };
+}
+
+
+// -----------------------------------------------------------------------------
+// Client Tasks
+// -----------------------------------------------------------------------------
+
+
+export interface Task {
+  id: string;
+  type: TaskType;
+  timeout: number;
+  timer: number;
+}
+
+
+export interface BookTask extends Task {
+  books: number;
+}
+
+
+function newBookPurchase(num: number, timeout: number, books: number): BookTask {
+  return {
+    id: num.toString(),
+    type: constTaskTypeBookPurchase(),
+    timeout,
+    timer: timeout,
+    books,
+  };
+}
+
+
+function newBookReturn(num: number, timeout: number, books: number): BookTask {
+  return {
+    id: num.toString(),
+    type: constTaskTypeBookReturn(),
+    timeout,
+    timer: timeout,
+    books,
+  };
+}
+
+
+// -----------------------------------------------------------------------------
+// Game State
+// -----------------------------------------------------------------------------
+
 
 export interface GameState {
   count: number;
   // General properties
-  players: Record<string, string>;
+  players: Record<string, PlayerState>;
   score: number;
   timer: number;
   // Pending player tasks
+  tasks: Array<Task>;
+  lastTaskCreatedAt: number;
   clientsPurchasing: Array<number>;   // clients waiting to purchase books
   clientsReturning: Array<number>;    // clients waiting to return books
   bookshelves: Array<Array<number>>;  // books on shelves
@@ -15,10 +96,6 @@ export interface GameState {
 
 type GameActions = {
   increment: (params: { amount: number }) => void;
-}
-
-declare global {
-  const Rune: RuneClient<GameState, GameActions>
 }
 
 export function getCount(game: GameState) {
@@ -40,6 +117,44 @@ function tickClientTasks(game, tasks) {
   }
 }
 
+
+function generateNewTask(game) {
+  // use randomness to determine task type
+  // use sorted insertion based on remaining time
+}
+
+
+function onGameTick(game) {
+  game.timer--;
+
+  const tasks = game.tasks;
+  for (let i = tasks.length - 1; i >= 0; --i) {
+    tasks[i]--;
+    if (tasks[i] < 0) {
+      // tasks are always sorted based on time left to complete them
+      // it is safe to discard all tasks below the last expired task
+      decreaseScore(game, (i+1) * 10);
+      tasks.splice(0, i+1);
+      break;
+    }
+  }
+
+  // use randomness to determine whether a new task will be generated
+  // use number of players and game.lastTaskCreatedAt to influence decision
+  const now = Rune.gameTimeInSeconds();
+  const reliefPeriod = now - game.lastTaskCreatedAt;
+  generateNewTask(game);
+}
+
+
+// -----------------------------------------------------------------------------
+// Rune Steup
+// -----------------------------------------------------------------------------
+
+declare global {
+  const Rune: RuneClient<GameState, GameActions>;
+}
+
 Rune.initLogic({
   minPlayers: 1,
   maxPlayers: 4,
@@ -50,13 +165,15 @@ Rune.initLogic({
       players: {},
       score: 0,
       timer: 300,
+      tasks: [],
+      lastTaskCreatedAt: 0,
       clientsPurchasing: [],
       clientsReturning: [],
       bookshelves: [],
       booksToReplace: [],
     };
     for (const id of allPlayerIds) {
-      state.players[id] = id;
+      state.players[id] = newPlayer(id);
     }
     return state;
   },
@@ -85,7 +202,9 @@ Rune.initLogic({
 
     addPendingPurchase: (_nothing, {game, playerId}) => {
       // a client that waits 10 seconds
+      game.count++;
       game.clientsPurchasing.push(10);
+      game.tasks.push(newBookPurchase(game.count, 10, 3));
     },
   },
 
